ded259c98f550e6f2b25e8608edc48aaad476efb
Kyle Liu <kyle116@yahoo.com>
2018-04-22 19:04:06 -0700

    signup form users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
+  
+  root 'users#index'
   get 'users/index'
 
   get 'users/show'
 
   get 'users/new'
 
-  get 'users/create'
+  post 'users/create'
 
   get 'users/edit'
 
   get 'users/destroy'
+  
+  # post 'users/create', as: 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
